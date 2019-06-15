f4a67353e1be84e0e0999e85ae4d93043070b056
Bob <bob.e.baxter@gmail.com>
2019-06-15 11:47:57 -0500

    watchlist and allmovies work, add to watchlist button and rating buttons now play well together

--- src/javascripts/components/watchlist/watchlist.js
+++ src/javascripts/components/watchlist/watchlist.js
@@ -1,44 +1,159 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import $ from 'jquery';
 
 import watchlistData from '../../helpers/data/watchlistData';
 import util from '../../helpers/util';
 import allMoviesData from '../../helpers/data/allMoviesData';
 import smash from '../../helpers/smash';
 
-const watchlistButtonEvents = (e) => {
-  const watchlistBtnBg = e.target.previousElementSibling.previousElementSibling;
-  const watchlistId = e.target.closest('span').id;
-  let starValue = $(e.target).closest('.movie-card').find('input:checked').val();
+const setIsWatchedStatus = (s) => {
   let isWatched;
+  let starValue = s;
   if (starValue === undefined) {
     starValue = null;
     isWatched = false;
   } else {
     isWatched = true;
   }
+  return isWatched;
+};
+
+const changeButtonToWatched = (e, watchlistBtnBg) => {
+  const buttonSpan = e.target.closest('span');
+  buttonSpan.classList.remove('off-watchlist');
+  buttonSpan.classList.add('on-watchlist');
+  e.target.classList.remove('fa-plus');
+  e.target.classList.add('fa-check');
+  watchlistBtnBg.classList.add('greenBtn');
+  // eslint-disable-next-line no-use-before-define
+  makeUniqueMovieList(firebase.auth().currentUser.uid);
+};
+
+const changeButtonToUnwatched = (e, watchlistBtnBg) => {
+  const buttonSpan = e.target.closest('span');
+  buttonSpan.classList.add('off-watchlist');
+  buttonSpan.classList.remove('on-watchlist');
+  // console.error(buttonSpan.classList);
+  e.target.classList.add('fa-plus');
+  e.target.classList.remove('fa-check');
+  watchlistBtnBg.classList.remove('greenBtn');
+  // eslint-disable-next-line no-use-before-define
+  makeUniqueMovieList(firebase.auth().currentUser.uid);
+};
+
+const watchlistButtonEvents = (e) => {
+  const watchlistBtnBg = e.target.previousElementSibling.previousElementSibling;
+  const movieId = e.target.closest('span').id;
+  const watchlistId = e.target.closest('.movie-card').id;
+  const starValue = $(e.target).closest('.movie-card').find('input:checked').val();
+  const isWatched = setIsWatchedStatus(starValue);
   if (e.target.classList.contains('fa-plus')) {
-    e.target.classList.remove('fa-plus');
-    e.target.classList.add('fa-check');
-    watchlistBtnBg.classList.add('greenBtn');
+    // builds movie object, then adds to or updates watchlist
+    console.error(watchlistId);
     const watchlistMovie = {
-      movieId: watchlistId.split('.')[1],
+      movieId: movieId.split('.')[1],
       uid: firebase.auth().currentUser.uid,
       rating: starValue,
       onWatchlist: true,
       isWatched,
     };
-    watchlistData.addToWatchlist(watchlistMovie);
+    if (watchlistId === 'undefined') {
+      // add if not in collection
+      watchlistData.addToWatchlist(watchlistMovie)
+        .then(changeButtonToWatched(e, watchlistBtnBg));
+    } else {
+      // update if in collection
+      watchlistData.editWatchlist(watchlistId, watchlistMovie)
+        .then(changeButtonToWatched(e, watchlistBtnBg));
+    }
+    // completion.then(changeButtonToWatched(e, watchlistBtnBg));
   } else {
-    e.target.classList.add('fa-plus');
-    e.target.classList.remove('fa-check');
-    watchlistBtnBg.classList.remove('greenBtn');
+    // builds movie object, then removes from or updates watchlist
+    const watchlistMovie = {
+      movieId: movieId.split('.')[1],
+      uid: firebase.auth().currentUser.uid,
+      rating: starValue,
+      onWatchlist: false,
+      isWatched,
+    };
+    // let completion;
+    if (watchlistId === 'undefined') {
+      // delete from collection if movie is not rated
+      watchlistData.removeFromWatchlist(watchlistId)
+        .then(() => {
+          // eslint-disable-next-line no-use-before-define
+          makeUniqueMovieList(firebase.auth().currentUser.uid);
+        })
+        .catch(err => console.error('didnt remove from watchlist', err));
+    } else {
+      // update onWatchlist to false if movie is rated
+      watchlistData.editWatchlist(watchlistId, watchlistMovie)
+        .then(() => changeButtonToUnwatched(e, watchlistBtnBg));
+    }
+    // completion.then(changeButtonToUnwatched(e, watchlistBtnBg));
   }
 };
 
 const radioButtonEvent = (e) => {
-  const rating = e.target;
-  console.error(rating);
+  const movieId = e.target.id.split('.')[1];
+  const watchlistBtn = $(e.target).closest('.movie-card').find('span')[0].childNodes[2];
+  // const movieId = e.target.closest('watchlist-button').id;
+  const starValue = $(e.target).closest('.movie-card').find('input:checked').val();
+  const watchlistId = e.target.closest('.movie-card').id;
+  const isWatched = setIsWatchedStatus(starValue);
+  // const onWatchlist = getWatchlistStatus(e);
+  if (watchlistBtn.classList.contains('fa-plus')) {
+    const watchlistMovie = {
+      movieId,
+      uid: firebase.auth().currentUser.uid,
+      rating: starValue,
+      onWatchlist: false,
+      isWatched,
+    };
+    if (watchlistId === 'undefined') {
+      // add if not in collection
+      watchlistData.addToWatchlist(watchlistMovie)
+        .then(() => {
+        // eslint-disable-next-line no-use-before-define
+          makeUniqueMovieList(firebase.auth().currentUser.uid);
+        });
+    } else {
+      // update if in collection
+      watchlistData.editWatchlist(watchlistId, watchlistMovie)
+        .then(() => {
+        // eslint-disable-next-line no-use-before-define
+          makeUniqueMovieList(firebase.auth().currentUser.uid);
+        });
+    }
+  } else {
+    // builds movie object, then removes from or updates watchlist
+    const watchlistMovie = {
+      movieId,
+      uid: firebase.auth().currentUser.uid,
+      rating: starValue,
+      onWatchlist: true,
+      isWatched,
+    };
+    // let completion;
+    if (watchlistId === 'undefined') {
+      // delete from collection if movie is not rated
+      watchlistData.removeFromWatchlist(watchlistId)
+        .then(() => {
+          // eslint-disable-next-line no-use-before-define
+          makeUniqueMovieList(firebase.auth().currentUser.uid);
+        })
+        .catch(err => console.error('didnt remove from watchlist', err));
+    } else {
+      // update onWatchlist to false if movie is rated
+      watchlistData.editWatchlist(watchlistId, watchlistMovie)
+        .then(() => {
+        // eslint-disable-next-line no-use-before-define
+          makeUniqueMovieList(firebase.auth().currentUser.uid);
+        });
+    // completion.then(changeButtonToUnwatched(e, watchlistBtnBg));
+    }
+  }
 };
 
 const eventListeners = () => {
@@ -55,7 +170,7 @@ const eventListeners = () => {
 const printWatchlist = (movies) => {
   let domString = '';
   movies.forEach((movie) => {
-    domString += '<div class="card movie-card bg-transparent border-0 p-0 col-3 m-2">';
+    domString += `<div id="${movie.watchlistId}" class="card movie-card bg-transparent border-0 p-0 col-3 m-2">`;
     domString += '<div class="card-title d-flex p-0 m-0 justify-content-center shadow">';
     domString += `<img src="${movie.imageUrl}" class="card-img-top"/>`;
     domString += `<span id="watchlist.${movie.id}" class="fa-stack topright watchlist-button off-watchlist">`;
@@ -64,7 +179,7 @@ const printWatchlist = (movies) => {
     domString += `<i class="fas ${movie.onWatchlist === true ? 'fa-check' : 'fa-plus'} fa-stack-1x"></i>`;
     domString += '</span>';
     domString += '</div>';
-    domString += '<div class="card-body d-flex p-1 justify-content-center align-items-end">';
+    domString += '<div class="card-body d-flex p-0 justify-content-center align-items-end">';
     domString += '<fieldset class="rating">';
     domString += `<input type="radio" id="star5.${movie.id}" name="rating.${movie.id}" value="5" class="ratingButton" ${movie.rating === '5' ? 'checked' : ''} /><label class="full" for="star5.${movie.id}" title="5 stars"></label>`;
     domString += `<input type="radio" id="star4.${movie.id}" name="rating.${movie.id}" value="4" class="ratingButton" ${movie.rating === '4' ? 'checked' : ''} /><label class="full" for="star4.${movie.id}" title="4 stars"></label>`;
